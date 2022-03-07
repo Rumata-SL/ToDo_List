36b481169c14a9ea4b9afa99eab49fd3a8f8811e
Rumata-SL <kirr7619@gmail.com>
2022-03-08 00:38:06 +0500

    Slight changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,24 @@
 import React, {useState} from "react";
-import './App.css';
-import TodoList, {TaskType} from "./TodoList";
+import "./App.css";
+import TodoList from "./TodoList";
 
 
 function App() {
 
-     const [tasks, setTask] = useState( [
+    const [tasks, setTask] = useState([
         {id: 1, title: "HTML&CSS", isDone: true},
         {id: 2, title: "JS", isDone: true},
         {id: 3, title: "React", isDone: false},
     ])
-    /*let tasks: Array<TaskType> = [
-        {id: 1, title: "HTML&CSS", isDone: true},
-        {id: 2, title: "JS", isDone: true},
-        {id: 3, title: "React", isDone: false},
-    ]*/
 
     const removeTask = ((id: number) => {
-        const filteredTasks = tasks.filter(t=> t.id !== id)
+        const filteredTasks = tasks.filter(t => t.id !== id)
         setTask(filteredTasks)
-        // tasks = tasks.filter(t => t.id !== id)
-
-
     })
+
     return (
         <div className="App">
             <TodoList title={"What to learn"} tasks={tasks} removeTask={removeTask}/>
-
         </div>
     );
 }
