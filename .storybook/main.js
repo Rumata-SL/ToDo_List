02edb5572deafd7b753eb66d5aeac642f6a98e33
Rumata-SL <kirr7619@gmail.com>
2022-05-24 22:14:12 +0500

    Added folder storybook, main.js, preview.js

---
+++ .storybook/main.js
@@ -0,0 +1,43 @@
+module.exports = {
+    "stories": [
+        "../src/**/*.stories.mdx",
+        "../src/**/*.stories.@(js|jsx|ts|tsx)"
+    ],
+    "addons": [
+        "@storybook/addon-links",
+        "@storybook/addon-essentials",
+        "@storybook/addon-interactions",
+        "@storybook/preset-create-react-app",
+        'storybook-dark-mode',
+        {
+            name: '@storybook/addon-docs',
+            options: {
+                sourceLoaderOptions: {
+                    injectStoryParameters: false,
+                },
+            },
+        },
+        {
+            name: '@storybook/addon-storysource',
+            options: {
+                rule: {
+                    test: [/\.stories\.tsx?$/]
+                },
+                loaderOptions: {
+                    prettierConfig: {
+                        printWidth: 80,
+                        singleQuote: false,
+                        options: {
+                            parser: 'typescript'
+                        },
+                        injectStoryParameters: false,
+                    },
+                },
+            },
+        },
+    ],
+    "framework": "@storybook/react",
+    "core": {
+        "builder": "@storybook/builder-webpack5"
+    }
+}
